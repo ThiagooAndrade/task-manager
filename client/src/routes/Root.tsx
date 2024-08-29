ca4c222c9d5075b0b3920e10ddefc336b83ace3e
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-29 17:43:50 -0300

    Root

---
+++ client/src/routes/Root.tsx
@@ -0,0 +1,11 @@
+import Menu from "../components/Menu";
+
+export default function Root() {
+
+
+    return (
+        <div className="container h-full bg-white">
+            <Menu />
+        </div>
+    )
+}
